5a90f1dbede7c41de416dd44f9e0114e07e2465d
neevan-gta99 <naveengta8581@gmail.com>
2026-02-22 17:54:43 +0530

    update fix in app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({              
-  origin: ['http://localhost:5174', "https://ott-server-x8tl.onrender.com"],
+  origin: ['http://localhost:5174', "https://ott-client-coral.vercel.app/"],
   credentials: true
 }));
 
